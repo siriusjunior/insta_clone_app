35f2c0451ff131beba99aa2689c269d793610a78
siriusjunior <topdownreading@gmail.com>
2021-08-27 06:07:47 +0900

    implement mail_on_activity with fullcode and later modify swiper_class

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
 
 
   config.session_store :redis_store, servers: 'redis://localhost:6379', expire_after: 1.day
+
+  config.action_mailer.default_url_options = Settings.default_url_options.to_h
+  config.action_mailer.delivery_method = :letter_opener_web
 end
