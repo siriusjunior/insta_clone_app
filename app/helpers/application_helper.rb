fbccd7741298f06d80fd7362fcd1e12195f601c7
siriusjunior <topdownreading@gmail.com>
2021-08-31 11:48:21 +0900

    finish metatag with ngrok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,25 @@
 module ApplicationHelper
-
+    def default_meta_tags
+        {
+            site: Settings.meta.site,
+            # サイト名 | タイトル名の並び順で出力するかどうか
+            reverse: true,
+            title: Settings.meta.title,
+            description: Settings.meta.description,
+            keywords: Settings.meta.keywords,
+            canonical: request.original_url,
+            og: {
+                title: :full_title,
+                type: Settings.meta.og.type,
+                url: request.original_url,
+                image: image_url(Settings.meta.og.image_path),
+                site_name: :site,
+                description: :description,
+                locale: 'ja_JP'
+            },
+            twitter: {
+                card: 'summary_large_image'
+            }
+        }
+    end
 end
